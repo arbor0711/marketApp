1aad74b4d1df4c089b56a01c1acd02e272d1a44c
Alireza <arbor0711@gmail.com>
2023-03-02 21:00:42 +0100

    Basic Setup

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+//integrate Mongoose
+const mongoose = require("mongoose");
+//Remember to run Mongo in the background
+mongoose
+  .connect("mongodb://127.0.0.1:27017/shopApp")
+  .then(() => console.log("Mongo Connection Open."))
+  .catch((err) => {
+    console.log("!!!Mongo Connection ERROR!!!");
+    console.log(err);
+  });
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+// Add basic routes
+app.get("/dog", (req, res) => {
+  res.send("woof");
+});
+
+app.listen(3000, () => {
+  console.log("App is listening on port 3000!");
+}); //to test that nodemon index.js
