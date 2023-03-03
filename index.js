fc5ba04ae30eae858ce847130d8742ef3667fab7
Alireza <arbor0711@gmail.com>
2023-03-03 12:01:01 +0100

    Creating a single page for a product details

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ app.get("/products", async (req, res) => {
   res.render("products/index", { products }); //it is possible to neglect .ejs at the end of index
 });
 
+// Add product details route
+// we cannot use the name of products for finding process, due to the similarity and more inportance for spaces inside the names, it is not safe for url that includes space.
+// we also should use a url that be unique and somehow readable for human. slugify
+// Composing a short but descriptive slug for a URL of the web page can positively affect your page's SEO
+// we ignore slugify here. OK!!!
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params; //destructuring object
+  const product = await Product.findById(id);
+  // render a template
+  res.render("products/show", { product }); //neglect .ejs
+});
+
 app.listen(3000, () => {
   console.log("App is listening on port 3000!");
 }); //to test that nodemon index.js
